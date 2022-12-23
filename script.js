cb6d0633d00a0488e65c8a3e0a11fb2a0556f06f
Egekorkmz <ege.korkmaz@ug.bilkent.edu.tr>
2022-12-23 15:01:39 +0300

    ğ

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 $(function(){
-    $("table")
+    //create letter boxes
+    var str = "";
+    for(let i = 0; i < 6; i++){
+        str += `<div class = "row">`
+        for(let k = 0; k < 6; k++){
+            str += `<div id = "${i}${k}"></div>`
+        }
+        str += "</div>";
+    }
+    $("#word").html(str);
+   
+
+    //display only vanted boxes
+    for(var i = 0; i < words.length; i++){
+        $(`#${words[i].id}`).text(`${words[i].value}`).css("visibility","visible");
+    }
+
 })
---
+++ words.js
@@ -0,0 +1,11 @@
+var words = [{id:"02" , value: 'V'}, {id:"03" , value: 'A'}, {id:"04" , value:'T'},
+            {id:"12" , value:'O' },{id:"22" , value:'T' },{id:"32", value:'E' },
+            {id:"20", value:'A' },{id:"21" , value:'R' },
+            {id:"31" , value:'M' },{id:"33" , value:'T' },{id:"34" , value:'A' },{id:"35" , value:'L' },
+            {id:"44" , value: 'G'},{id:"54" , value:'E' },
+            {id:"50" , value:'P' },{id:"51" ,value: 'A' },{id:"52" , value: 'U' },{id:"53" , value:'S' }];
+
+
+var word = ["vat", "vote", "metal", "art", "age", "pause"]
+
+var letter = ['V', 'A', 'T', 'O', 'E', 'R', 'M', 'L', 'G', 'P', 'U', 'S'];
