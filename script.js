706780e52f523c1765f0cc19eaa01fa9bb47976d
Egekorkmz <ege.korkmaz@ug.bilkent.edu.tr>
2022-12-24 20:37:33 +0300

    lelü

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ $(function(){
         }
         str += "</div>";
     }
-    $("#word").html(str);
+    $("#words").html(str);
    
 
     //display only vanted boxes
@@ -28,26 +28,47 @@ $(function(){
     var arr = new Array();
     //create letters
     for(var i = 0; i < 6; i++){
-        
         for(let i = 0; i < arr.length; i++){
-            console.log(arr[i] + " " + number);
             while(number === arr[i]){
                 number = Math.floor(Math.random()*letter.length);
                 i = 0;
-                console.log("its working")
             }   
         }
         arr.push(number);
         str += `<div id = "${i}">${letter[number]}</div>`
-        number = Math.floor(Math.random()*letter.length);
-        
-        
+        number = Math.floor(Math.random()*letter.length);  
     }
-    $("#widget").html(str); 1
+    $("#widget").append(str);
     
     //positionings
     for(var i = 0; i < 6; i++){
         $(`#${i}`).css({"left" : `${145+ Math.cos(convert(60-60*i))*100}px`, "bottom" : `${145 +Math.sin(convert(60-60*i))*100}px`});
     }
+
+    $("#widget").children().each(function(){
+        $(this).click(function(){
+            var temp = $(this).css("background-color", "red").text();
+            if(temp !== "")
+                $("#word").append(temp).css("visibility", "visible");
+        })
+    })
     
+    $("#shuffle").click(function(){
+        number = Math.floor(Math.random()*letter.length);
+        arr = new Array();
+        for(var i = 0; i < 6; i++){
+            for(let i = 0; i < arr.length; i++){
+                while(number === arr[i]){
+                    number = Math.floor(Math.random()*letter.length);
+                    i = 0;
+                }   
+            }
+            arr.push(number);
+            $(`#${i}`).text(`${letter[number]}`)
+            number = Math.floor(Math.random()*letter.length)
+        }
+
+        $("#widget").children().each(function(){$(this).css("background-color", "aliceblue")})
+        $("#word").text("").css("visibility", "hidden")
+    })
 })
